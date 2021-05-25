339b9ce3cc0009b3e7d43b9d949544e6fe833d45
MoltenCoffee <13321277+MoltenCoffee@users.noreply.github.com>
2021-05-25 13:20:11 +0200

    Fix link styles, make header link

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,18 @@ import { FormattedMessage } from "react-intl";
 import Heading from "./Heading";
 import styles from "./header.module.css";
 import GitHub from "./GitHub";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className={styles.wrapper}>
       <Heading>
-        <FormattedMessage id="title" defaultMessage="Space Haven Save Editor" />
+        <NavLink to="/">
+          <FormattedMessage
+            id="title"
+            defaultMessage="Space Haven Save Editor"
+          />
+        </NavLink>
       </Heading>
       <GitHub />
     </header>
