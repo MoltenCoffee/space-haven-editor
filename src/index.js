5046e0bfb933c76da31b93daec2cac3008a8646e
MoltenCoffee <13321277+MoltenCoffee@users.noreply.github.com>
2022-05-01 13:01:59 +0200

    Remove Analytics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from './reportWebVitals';
-import sendToGoogleAnalytics from "./lib/sendToGoogleAnalytics";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
 
 serviceWorkerRegistration.unregister();
-reportWebVitals(sendToGoogleAnalytics);
